58671e40e85fd747d41881284cbc7c7aac7205b6
abdulWasayWaqas <143158580+abdulWasayWaqas@users.noreply.github.com>
2024-05-12 21:14:10 +0500

    Update register.dart

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 class MyRegister extends StatefulWidget {
@@ -13,7 +15,7 @@ class _MyRegisterState extends State<MyRegister> {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-            image: AssetImage('register.png'), fit: BoxFit.cover),
+            image: AssetImage('assets/register.png'), fit: BoxFit.cover),
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
