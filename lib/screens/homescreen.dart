436e0a76dd8540df3cec2d89dc4e301446cd2fca
abdulWasayWaqas <143158580+abdulWasayWaqas@users.noreply.github.com>
2024-05-17 18:49:11 +0500

    Update homescreen.dart

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,33 +1,94 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:first_app/common/bottombar.dart';
 import 'package:first_app/common/menudrawer.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Fitness App"),
-          backgroundColor: Color.fromARGB(103, 192, 64, 156),
+      appBar: AppBar(
+        title: Text("Fitness App"),
+        backgroundColor: Colors.orange,
+      ),
+      drawer: const MenuDrawer(),
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, left: 20.0),
+              child: Text(
+                "Welcome!",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: BubbleAnimation(),
+              ),
+            ),
+          ],
         ),
-        drawer: const MenuDrawer(),
-        bottomNavigationBar: MyBottomNavBar(),
-        body: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage("assets/home.png"))),
-            child: Center(
-                child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Color.fromARGB(103, 192, 64, 156)),
-                    child: Text(
-                      "Stay Fit and have a Healthy Life",
-                      style: TextStyle(fontSize: 15),
-                    )))));
+      ),
+    );
   }
 }
+
+class BubbleAnimation extends StatefulWidget {
+  @override
+  _BubbleAnimationState createState() => _BubbleAnimationState();
+}
+
+class _BubbleAnimationState extends State<BubbleAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _bubbleScaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    );
+    _bubbleScaleAnimation =
+        Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Interval(0.0, 0.5, curve: Curves.easeInOut),
+    ));
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Opacity(
+          opacity: _bubbleScaleAnimation.value,
+          child: Text(
+            "Stay Fit and have\na Healthy Life",
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.orange,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        );
+      },
+    );
+  }
+}
+
